7e5e9e7115ae9806c52c7a429606d2bde2b8c553
VslVictor7 <vic.collfac358@outlook.com>
2025-01-26 00:19:28 -0300

    feat: hold button for 1 second and then releases it

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import pyautogui
 import time
 
-intervalo = 4
+intervalo = 7
 
 try:
     while True:
-        pyautogui.click(button='left')
-        print("Clique realizado.")
+        pyautogui.mouseDown(button='left')
+        print("Botão pressionado.")
+        time.sleep(1)
+        pyautogui.mouseUp(button='left')
+        print("Botão solto.")
         time.sleep(intervalo)
 except KeyboardInterrupt:
     print("Execução interrompida pelo usuário.")
